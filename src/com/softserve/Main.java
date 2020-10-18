0bb41c216cb59f33768945a663baaf3988762c1c
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-10-19 02:15:15 +0300

    Add files via upload

---
+++ src/com/softserve/Main.java
@@ -0,0 +1,20 @@
+package com.softserve;
+
+public class Main {
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+        int quotient = 0;
+
+        quotient = dividend / divisor;
+        System.out.println(quotient);
+    }
+}
+
+
+
+
+
+
+
+
